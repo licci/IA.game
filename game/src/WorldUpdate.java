842ae06f466e8efb02306bbd97c16940953dcfeb
Maciej <thamiar@gmail.com>
2013-11-15 12:21:15 +0100

    Random road changes

--- game/src/WorldUpdate.java
+++ game/src/WorldUpdate.java
@@ -7,8 +7,8 @@ public class WorldUpdate {
 
 	 static int times = 0;
 	
-	
-	
+	static int roadTimer=0;
+	static int roadRand=0;
 	 static void Update()
 		{
 		 
@@ -29,9 +29,8 @@ public class WorldUpdate {
 				case 3: if(times==3||times==6) Add(type,i);
 				}
 				
-				
-				
 			}
+			dynamicRoad();
 		}
 	 
 	
@@ -58,4 +57,51 @@ public class WorldUpdate {
 	            	break;
 	    }
 	    }
+	 
+	 static void dynamicRoad()
+	 {
+		 roadTimer++;
+			if(roadTimer==1)
+			{ 
+				roadRand=(int) (Math.random() * 14);
+				switch(roadRand)
+				{
+				case 0: graph.map[1].changeRoadState(4,false); break;
+				case 1: graph.map[4].changeRoadState(2,false); break;
+				case 2: graph.map[2].changeRoadState(6,false); break;
+				case 3: graph.map[3].changeRoadState(6,false); break;
+				case 4: graph.map[3].changeRoadState(5,false); break;
+				case 5: graph.map[4].changeRoadState(8,false); break;
+				case 6: graph.map[5].changeRoadState(7,false); break;
+				case 7: graph.map[5].changeRoadState(8,false); break;
+				case 8: graph.map[5].changeRoadState(9,false); break;
+				case 9: graph.map[6].changeRoadState(8,false); break;
+				case 10: graph.map[8].changeRoadState(10,false); break;
+				case 11: graph.map[7].changeRoadState(11,false); break;
+				case 12: graph.map[7].changeRoadState(10,false); break;
+				case 13: graph.map[8].changeRoadState(12,false); break;
+				case 14: graph.map[9].changeRoadState(11,false); break;
+				}
+			}else if(roadTimer==5){
+				roadTimer=0;
+				switch(roadRand)
+				{
+				case 0: graph.map[1].changeRoadState(4,true); break;
+				case 1: graph.map[4].changeRoadState(2,true); break;
+				case 2: graph.map[2].changeRoadState(6,true); break;
+				case 3: graph.map[3].changeRoadState(6,true); break;
+				case 4: graph.map[3].changeRoadState(5,true); break;
+				case 5: graph.map[4].changeRoadState(8,true); break;
+				case 6: graph.map[5].changeRoadState(7,true); break;
+				case 7: graph.map[5].changeRoadState(8,true); break;
+				case 8: graph.map[5].changeRoadState(9,true); break;
+				case 9: graph.map[6].changeRoadState(8,true); break;
+				case 10: graph.map[8].changeRoadState(10,true); break;
+				case 11: graph.map[7].changeRoadState(11,true); break;
+				case 12: graph.map[7].changeRoadState(10,true); break;
+				case 13: graph.map[8].changeRoadState(12,true); break;
+				case 14: graph.map[9].changeRoadState(11,true); break;
+				}
+			}
+	 }
 }
